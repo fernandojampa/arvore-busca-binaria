1f0c40bc885edc33f4bc454cdf8f56882ebd7f06
Fernando Borges <fernandoborgesjp@gmail.com>
2021-09-20 00:13:10 -0300

    Lógica dos nós na árvore abb

--- arvoreBinaria.py
+++ arvoreBinaria.py
@@ -1,6 +1,10 @@
+from node import Node
+
+
 class BinaryTree:
     def __init__(self):
         self._frase = []
+        #self._abb = []
 
     def em_ordem(self, arvore):
         if arvore != None:
@@ -11,4 +15,11 @@ class BinaryTree:
     def split(self, frase):
         self._frase = frase.split()
         for i in range(len(self._frase)):
-            print(self._frase[i])
+            no = Node()
+            if i == 0:
+                no.__pai = self._frase[i]
+            elif i > 0:
+                if self._frase[i] < no.__pai:
+                    no.__esq = self._frase[i]
+                elif self._frase[i] > no.__pai:
+                    no.__dir = self._frase[i]
